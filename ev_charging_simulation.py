1e83b3ff90ae25d4dbcdfce0f6bdd0b3c9e7a3a7
Fatma Akarsu <79910837+Fatmaakarsu@users.noreply.github.com>
2023-12-17 23:23:19 +0300

    Update ev_charging_simulation.py

--- ev_charging_simulation.py
+++ ev_charging_simulation.py
@@ -2,59 +2,88 @@ import random
 import time
 
 class ChargingStation:
-    def __init__(self, cs_id, location):
+    def __init__(self, cs_id, location, capacity):
         self.cs_id = cs_id
-        self.location = location  # Şarj istasyonunun konumu
-        self.available_slots = 5  # Her şarj istasyonunda 5 şarj yuvası
-        self.last_access_time = time.time()  # Son erişim zamanı
+        self.location = location
+        self.capacity = capacity
+        self.available_slots = capacity
+        self.last_access_time = time.time()
 
     def check_traffic_anomaly(self):
         current_time = time.time()
         time_diff = current_time - self.last_access_time
 
-        # Eğer belirli bir süre içinde çok sayıda erişim varsa, bu anormal bir durum olarak değerlendirilebilir.
         if time_diff < 5 and self.available_slots < 3:
             print(f"Anomaly detected at CS {self.cs_id} in {self.location}. Possible DoS or DDoS attack.")
 
 class ElectricVehicle:
-    def __init__(self, ev_id):
+    def __init__(self, ev_id, location, model, charging_time):
         self.ev_id = ev_id
+        self.location = location
+        self.model = model
+        self.charging_time = charging_time
 
 class GridStation:
     def __init__(self):
         self.cs_list = []
 
-def create_simulation_environment(num_cs, num_ev):
+def create_simulation_environment(cs_data, ev_data):
     grid_station = GridStation()
 
-    locations = ["Downtown", "Suburb", "Airport", "Shopping Mall", "Residential Area"]
-
-    for i in range(num_cs):
-        cs = ChargingStation(cs_id=f"CS{i+1}", location=random.choice(locations))
+    for cs_info in cs_data:
+        cs = ChargingStation(cs_id=cs_info["cs_id"], location=cs_info["location"], capacity=cs_info["capacity"])
         grid_station.cs_list.append(cs)
 
-    ev_list = [ElectricVehicle(ev_id=f"EV{i+1}") for i in range(num_ev)]
+    ev_list = [ElectricVehicle(ev_id=ev_info["ev_id"], location=ev_info["location"], model=ev_info["model"], charging_time=ev_info["charging_time"]) for ev_info in ev_data]
 
     return grid_station, ev_list
 
 def simulate_charging(grid_station, ev_list):
+    suspect_vehicles = []  # Şüpheli araçları saklamak için bir dizi
+
     for ev in ev_list:
         selected_cs = random.choice(grid_station.cs_list)
 
         if selected_cs.available_slots > 0:
-            print(f"EV {ev.ev_id} charging at CS {selected_cs.cs_id} in {selected_cs.location}")
+            print(f"EV {ev.ev_id} (Model: {ev.model}) charging at CS {selected_cs.cs_id} in {selected_cs.location} for {ev.charging_time} minutes")
             selected_cs.available_slots -= 1
-            selected_cs.last_access_time = time.time()  # Güncellenmiş erişim zamanı
-            selected_cs.check_traffic_anomaly()  # Trafik anormalliğini kontrol et
+            selected_cs.last_access_time = time.time()
+            selected_cs.check_traffic_anomaly()
+
+            if ev.charging_time > 60:
+                suspect_vehicles.append(ev.ev_id)
+
             time.sleep(random.uniform(1, 5))
             selected_cs.available_slots += 1
         else:
             print(f"No available slots at CS {selected_cs.cs_id} in {selected_cs.location} for EV {ev.ev_id}")
 
+    return suspect_vehicles  # Şüpheli araçları döndür
+
 if __name__ == "__main__":
-    num_charging_stations = 3
-    num_electric_vehicles = 5
+    # Güncellenmiş veri seti
+    cs_data = [
+        {"cs_id": "CS1", "location": "Downtown", "capacity": 5},
+        {"cs_id": "CS2", "location": "Suburb", "capacity": 5},
+        {"cs_id": "CS3", "location": "Airport", "capacity": 5}
+    ]
+    ev_data = [
+        {"ev_id": "EV1", "location": "Downtown", "model": "Model1", "charging_time": random.randint(30, 100)},
+        {"ev_id": "EV2", "location": "Suburb", "model": "Model2", "charging_time": random.randint(30, 100)},
+        {"ev_id": "EV3", "location": "Airport", "model": "Model3", "charging_time": random.randint(30, 100)},
+        {"ev_id": "EV4", "location": "Downtown", "model": "Model4", "charging_time": random.randint(30, 100)},
+        {"ev_id": "EV5", "location": "Suburb", "model": "Model5", "charging_time": random.randint(30, 100)}
+    ]
 
-    simulation_grid, evs = create_simulation_environment(num_charging_stations, num_electric_vehicles)
+    simulation_grid, evs = create_simulation_environment(cs_data, ev_data)
 
-    simulate_charging(simulation_grid, evs)
+    suspect_vehicles = simulate_charging(simulation_grid, evs)
+
+    print("\nAll Electric Vehicles:")
+    for ev in evs:
+        print(f"EV {ev.ev_id} (Model: {ev.model}) - Charging time: {ev.charging_time} minutes")
+
+    if suspect_vehicles:
+        print("\nWarning: Charging time more than 60 minutes! Suspect Vehicles:", ", ".join(suspect_vehicles))
+    else:
+        print("\nNo suspect vehicles detected.")
